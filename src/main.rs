827ab73fe8181534e5f9340a0af8410602d6200f
Uditt Lamba <uditt.lamba@pm.me>
2022-01-20 03:59:58 +0530

    dockerized the app. todo: get pgsql container to work

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 mod routes;
 use rocket::routes;
-use crate::routes::{test::get_hello_world, home::get_home};
+use crate::routes::{test, home};
 
 #[rocket::main]
 async fn main() {
-    rocket::build()
-        .mount("/hello", routes![get_hello_world])
-        .mount("/", routes![get_home])
+    // let figment = rocket::Config::figment()
+    //     .merge(Toml::file("../Rocket.toml"));
+
+    let result = rocket::build()
+        .mount("/hello", routes![test::get_hello_world])
+        .mount("/", routes![home::get_home])
         .launch()
         .await;
+
+    match result {
+        Ok(result) => println!("app launched successfully {:?}", result),
+        Err(error) => panic!("failed to launch with error {:?}", error),
+    }
 }
