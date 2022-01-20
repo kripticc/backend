0ef7e66013910a33f80dd915d8c3632deda4bbd8
Uditt Lamba <uditt.lamba@pm.me>
2022-01-20 11:38:48 +0530

    gitignore update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # backend-rust
 experimenting with rust at the backend
+
+## Starting the docker containers
+### Build the container with the following:
+`sudo docker-compose -f docker-compose.dev.yml build`
+### Run the containers with the following:
+`sudo docker-compose -f docker-compose.dev.yml up`
--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,14 @@ async fn main() {
     // let figment = rocket::Config::figment()
     //     .merge(Toml::file("../Rocket.toml"));
 
-    let result = rocket::build()
+    rocket::build()
         .mount("/hello", routes![test::get_hello_world])
         .mount("/", routes![home::get_home])
         .launch()
-        .await;
+        .await?
 
-    match result {
-        Ok(result) => println!("app launched successfully {:?}", result),
-        Err(error) => panic!("failed to launch with error {:?}", error),
-    }
+    // match result {
+    //     Ok(result) => println!("app launched successfully {:?}", result),
+    //     Err(error) => panic!("failed to launch with error {:?}", error),
+    // }
 }
