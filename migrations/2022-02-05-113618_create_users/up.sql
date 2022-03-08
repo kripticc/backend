4109eda043de9ecd56b39454deb15d2d9e407680
Uditt Lamba <uditt.lamba@pm.me>
2022-03-09 02:54:37 +0530

    continue from  db for users

--- migrations/2022-02-05-113618_create_users/up.sql
+++ migrations/2022-02-05-113618_create_users/up.sql
@@ -1,11 +1,11 @@
 CREATE TABLE users (
-                       id SERIAL PRIMARY KEY ,
-                       user_name VARCHAR UNIQUE NOT NULL,
+                       user_name VARCHAR(255) PRIMARY KEY,
                        salt VARCHAR NOT NULL,
                        email_hash VARCHAR NOT NULL,
                        password_hash VARCHAR NOT NULL,
                        profile_pic VARCHAR UNIQUE NOT NULL,
                        bio TEXT,
                        created_at TIMESTAMP NOT NULL,
-                       updated_at TIMESTAMP NOT NULL
+                       updated_at TIMESTAMP NOT NULL,
+                       deleted_at TIMESTAMP NOT NULL
 )
--- src/routes/home.rs
+++
@@ -1,6 +0,0 @@
-use rocket::get;
-
-#[get("/")]
-pub fn get_home() -> &'static str {
-    "This is home."
-}
